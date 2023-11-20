0320872b2206e0a6ebb98b347048ea6e85e213d0
gachempa <jpsg514@gmail.com>
2023-11-19 18:19:44 -0800

    Started coding stock sale problem

---
+++ stockbuysellleetcode.py
@@ -0,0 +1,7 @@
+stockDailyPrices = [2,3,4,5,4,3,3,5,3,6,2,4]
+numberOfTrades = 2
+
+len_stockDailyPrices = len(stockDailyPrices)
+
+print("Daily price of stock = ", stockDailyPrices)
+print("Length of stockDailyPrices = ", len_stockDailyPrices)
