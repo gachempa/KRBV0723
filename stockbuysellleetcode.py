f5d33cc5fad67048dc0eef0cd126eda084c04482
gachempa <jpsg514@gmail.com>
2023-11-21 12:10:06 -0800

    collapsible trades captured

--- stockbuysellleetcode.py
+++ stockbuysellleetcode.py
@@ -2,7 +2,7 @@
 # input list of stock prices, and number of trades allowed 
 # for iniital testing
 # actual problem will use auto-inputs from source 
-stockDailyPrices = [2,3,4,5,4,3,3,5,3,6,2,4]
+stockDailyPrices = [2,3,4,5,4,3,3,5,3,6,2,4,3,5]
 numberOfTrades = 2
 
 # the size of the list or number of values provided 
@@ -37,7 +37,14 @@ print("Length of stockDailyPrices = ", len_stockDailyPrices)
 print("Value of all_Trades (before collpsing) = ", all_trades)
 
 # program below is to collapse the output down to problem's allowed trades
-
+# capture 1st trade index, value created or lost
+collapsible_trades=[]
 for x in range(len(all_trades)):
-    if x+1<len(all_trades) and all_trades[x][2]<=all_trades[(x+1)][2]:
-        print("for trade", x+1, ":", "sell price not higher than next trade")
+    # two trades can be combined if buy1<=buy2 and sell1<sell2
+    if x+1<len(all_trades) and all_trades[x][1]<=all_trades[(x+1)][1] and all_trades[x][2]<all_trades[(x+1)][2]:
+        print("Trades", x, "and", x+1, "can be collapsed if needed")
+        valueCreatedOnCollapse=(all_trades[x+1][2]-all_trades[x][1]-(all_trades[x][3]+all_trades[x+1][3]))
+        thisTradeValue=[x,x+1,valueCreatedOnCollapse]
+        print("Collapsible trade:",thisTradeValue)
+    # elif (all_trades[(x+1)][2]-all_trades[x][1])>(all_trades[(x+1)][3]+all_trades[x][3]):
+    #     print("")
