cc17ee99f7b755f685a88380705874203c099855
gachempa <jpsg514@gmail.com>
2023-12-01 20:07:38 -0800

    started sample plotting...

--- pandasnumpytrials.py
+++ pandasnumpytrials.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 
+# Machine Learning with Python and Scikit-Learn – Full Course
+# https://www.youtube.com/watch?v=hDKCxebp88A&ab_channel=freeCodeCamp.org
+
 medical_df = pd.read_csv('medical.csv')
 
 # print(medical_df.info())
-print(medical_df.describe())
+# print(medical_df.age.describe())
+fig = px.histogram(medical_df,x='age',marginal='box',nbins=47,title='Distribution of Age')
+fig.update_layout(bargap=0.1)
+fig.show()
