1a64f5a586388d4aa1e15229c4da0dd16b28e55c
gachempa <jpsg514@gmail.com>
2023-12-13 20:01:52 -0800

    list to track all windows

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -4,6 +4,15 @@
 s = "ADOBECODEBANC"
 t = "ABC"
 
+# a list to hold all possible windows
+all_possible_windows=[]
+# each item in the list above has start(index,value),final(index,value)
+# all_possible_windows_item=[]
+
 for count, x in enumerate(s):
     if x in t:
+        all_possible_windows_item=(count,x,count,x)
+        all_possible_windows.append(all_possible_windows_item)
         print(count,x)
+
+print(len(all_possible_windows))
