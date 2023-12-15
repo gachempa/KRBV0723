7de851497c5d2ba06e14ede5e8a82438cffe2754
gachempa <jpsg514@gmail.com>
2023-12-15 07:50:00 -0800

    working on create window at specific index

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -21,11 +21,22 @@ def pop_from_string(the_string, pop_what):
     new_string=the_string.replace(pop_what[1], "", 1)
     return new_string
 
-# take the list of all possible windows, which window index to use (index, character)
+# function to find window from a specific starting point
+def window_from_any_index(start_index, full_string,window_for_index):
+    start_from=start_index[0]
+    for x in range(start_from,len(full_string)):
+        print(x)
+
+# fn takes the list of all possible windows, which window index to use (index, character)
 # and the window (remove the start-character then iterate and find other characters)
 def create_windows(start_list,which_window_index,t_window):
     new_t_window=pop_from_string(t_window,which_window_index)
     print(new_t_window)
+
+    # for x in range(len(start_list)-len(t_window)):
+    #     print(x)
+
+    window_from_any_index(start_list[0],start_list,new_t_window)
 
 create_windows(all_possible_windows,all_possible_windows[1],t)
 
