8311aa3fe08cf350e30f65a88e0be15a6ff41dbc
gachempa <jpsg514@gmail.com>
2023-12-16 09:16:52 -0800

    improvements and simplifications

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -4,17 +4,20 @@
 s = "ADOBECODEBANC"
 t = "ABC"
 
-# a list to hold all possible windows
+the_minimum_window=""
+
+# a list to hold all possible windows, removes non-t alphabets
 all_possible_windows=[]
 
 for count, x in enumerate(s):
     if x in t:
-        # all_possible_windows_item=[start(index,value),final(index,value)]
-        all_possible_windows_item=(count,x,count,x)
+        # all_possible_windows_item=[start(index,value),final(index,value),0/1]
+        # 0 is for incomplete windowa and 1 is for complete window
+        all_possible_windows_item=(count,x,count,x,0)
         all_possible_windows.append(all_possible_windows_item)
-        print(count,x)
+        # print(count,x)
 
-print(len(all_possible_windows))
+print(all_possible_windows)
 
 # pop an item from a string
 def pop_from_string(the_string, pop_what):
@@ -25,19 +28,19 @@ def pop_from_string(the_string, pop_what):
 def window_from_any_index(start_index, full_string,window_for_index):
     start_from=start_index[0]
     for x in range(start_from,len(full_string)):
-        print(x)
-        print(window_for_index)
+        print(x,full_string[x])
+        # print(window_for_index)
 
 # fn takes the list of all possible windows, which window index to use (index, character)
 # and the window (remove the start-character then iterate and find other characters)
-def create_windows(start_list,which_window_index,t_window):
-    new_t_window=pop_from_string(t_window,which_window_index)
+def create_windows(all_possible_windows_,which_window_index,t_):
+    new_t_window=pop_from_string(t_,which_window_index)
     print(new_t_window)
 
     # for x in range(len(start_list)-len(t_window)):
     #     print(x)
 
-    window_from_any_index(start_list[0],start_list,new_t_window)
+    window_from_any_index(all_possible_windows_[0],all_possible_windows_,new_t_window)
 
 create_windows(all_possible_windows,all_possible_windows[1],t)
 
