5794dc928a37429692670eda1a6cb67fe67bcd82
gachempa <jpsg514@gmail.com>
2023-12-16 16:43:39 -0800

    ready for leetcode submission

--- MinimumWindowSubstring-LC-2.py
+++ MinimumWindowSubstring-LC-2.py
@@ -22,7 +22,7 @@ else:
             all_possible_windows.append(all_possible_windows_item)
             # print(count,x)
 
-    print(all_possible_windows)
+    # print(all_possible_windows)
 
     # pop an item from a string
     def pop_from_string(the_string, pop_what):
@@ -45,9 +45,9 @@ else:
     # and the window (remove the start-character then iterate and find other characters)
     def create_windows(x_,all_possible_windows_,which_window_index,t_):
         new_t_window=pop_from_string(t_,which_window_index)
-        print(new_t_window)
+        # print(new_t_window)
         a=all_possible_windows_[x_]
-        print(a)
+        # print(a)
 
         window_end=window_from_any_index(x_,all_possible_windows_,new_t_window)
         # print("end index returned",window_end)
@@ -57,19 +57,19 @@ else:
     for x in range (len(all_possible_windows)):
         # this will return the end index of window
         window_end=create_windows(x,all_possible_windows,all_possible_windows[x],t)
-        print("window-end is",window_end)
+        # print("window-end is",window_end)
         if the_minimum_window=="" and window_end is not None:
             s_start_index=x
             s_end_index=all_possible_windows[window_end][0]
             the_minimum_window=s[s_start_index:s_end_index+1]
-            print("min window is",the_minimum_window)
+            # print("min window is",the_minimum_window)
         elif window_end is not None:
             s_start_index=all_possible_windows[x][0]
             s_end_index=all_possible_windows[window_end][0]
             new_window=s[s_start_index:s_end_index+1]
-            print("min window is",new_window)
+            # print("min window is",new_window)
             if len(new_window)<len(the_minimum_window):
                 the_minimum_window=new_window
-                print("updated new min window is",the_minimum_window)
+                # print("updated new min window is",the_minimum_window)
 
-    print("Final, minimum window:",the_minimum_window)
+    print(the_minimum_window)
