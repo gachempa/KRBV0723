ae521e117f9a7af5529c88c151a44738f7aa319b
gachempa <jpsg514@gmail.com>
2023-12-30 12:52:08 -0800

    some ocr work - using tesseract

---
+++ MachineLearning/gc-ocr.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+import pytesseract
+
+# pytesseract.pytesseract.tesseract_cmd = r'/Users/home_dada/Library/Python/3.9/bin'
+
+print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/mini-test.png')))
