a5fd0c6497cdad6a0f49d3c8345190ed2bd7e928
gachempa <jpsg514@gmail.com>
2023-12-31 10:26:11 -0800

    tested tryst with destinuy

--- MachineLearning/gc-ocr.py
+++ MachineLearning/gc-ocr.py
@@ -4,4 +4,6 @@ import pytesseract
 
 # pytesseract.pytesseract.tesseract_cmd = r'/Users/home_dada/Library/Python/3.9/bin'
 
-print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/mini-test.png')))
+print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/trystwithdestiny.png')))
+
+# print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/mini-test.png')))
