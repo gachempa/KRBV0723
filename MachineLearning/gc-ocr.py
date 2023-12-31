f63a0b3d5b92e57b8dac4c20c284592d37dd1f9f
gachempa <jpsg514@gmail.com>
2023-12-31 11:00:20 -0800

    image library folder

--- MachineLearning/gc-ocr.py
+++ MachineLearning/gc-ocr.py
@@ -4,6 +4,6 @@ import pytesseract
 
 # pytesseract.pytesseract.tesseract_cmd = r'/Users/home_dada/Library/Python/3.9/bin'
 
-print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/trystwithdestiny.png')))
+print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/ImageLibrary/trystwithdestiny.png')))
 
-# print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/mini-test.png')))
+# print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/ImageLibrary/mini-test.png')))
