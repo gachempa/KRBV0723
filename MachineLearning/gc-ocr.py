62d54e024cfeb72be809bfbeb308d93c435cf54d
gachempa <jpsg514@gmail.com>
2024-01-01 10:55:40 -0800

    png of tabular data OCR poor

--- MachineLearning/gc-ocr.py
+++ MachineLearning/gc-ocr.py
@@ -4,6 +4,8 @@ import pytesseract
 
 # pytesseract.pytesseract.tesseract_cmd = r'/Users/home_dada/Library/Python/3.9/bin'
 
-print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/ImageLibrary/trystwithdestiny.png')))
+# print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/ImageLibrary/trystwithdestiny.png')))
+
+print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/ImageLibrary/jose-11-2022.png')))
 
 # print(pytesseract.image_to_string(Image.open('/Users/home_dada/BobbyProgramming/KRBV0723-1/MachineLearning/ImageLibrary/mini-test.png')))
