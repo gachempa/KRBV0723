c0920207d42dab150b564a10994650ab57b4e867
gachempa <jpsg514@gmail.com>
2024-01-15 12:02:17 -0800

    size of LL function

--- Merge_k_SortedList.py
+++ Merge_k_SortedList.py
@@ -1,19 +1,20 @@
-# /**
-#  * Definition for singly-linked list.
-#  * struct ListNode {
-#  *     int val;
-#  *     ListNode *next;
-#  *     ListNode() : val(0), next(nullptr) {}
-#  *     ListNode(int x) : val(x), next(nullptr) {}
-#  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-#  * };
-#  */
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 """
-class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-    }
-};
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
 """
+def sizeOfLL(self):
+	size = 0
+	if(self.head):
+		current_node = self.head
+		while(current_node):
+			size = size+1
+			current_node = current_node.next
+		return size
+	else:
+		return 0
