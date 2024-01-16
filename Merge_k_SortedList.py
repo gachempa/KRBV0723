44da496da143fb1a072647aec66e969bf80b996a
gachempa <jpsg514@gmail.com>
2024-01-16 06:24:42 -0800

    i think understood the input reqs

--- Merge_k_SortedList.py
+++ Merge_k_SortedList.py
@@ -23,3 +23,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+		
+# code to locally test data
+# make this a linked list with name ListNode
+given_list=[[1,4,5],[1,3,4],[2,6]]
