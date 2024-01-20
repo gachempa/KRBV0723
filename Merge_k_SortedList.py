1f8b4133918377131af34bd7683ab9a9adef2114
gachempa <jpsg514@gmail.com>
2024-01-20 15:28:24 -0800

    working thru linked lists

--- Merge_k_SortedList.py
+++ Merge_k_SortedList.py
@@ -20,7 +20,30 @@ given_list=[[1,4,5],[1,3,4],[2,6]]
 
 class LinkedList:
     def __init__(self) -> None:
-		self.head=None
-            
-	def insert_at_beginning(self,val):
-          listNode=ListNode(val,self.head)
+        self.head=None
+    
+    def insert_at_beginning(self,val):
+        listNode=ListNode(val,self.head)
+        self.head=listNode
+
+    def print(self):
+        if self.head is None:
+            print('Linked list is empty')
+            return
+        
+        itr=self.head
+        llstr=''
+
+        while itr:
+            llstr += str(itr.data)+'-->'
+            itr=itr.next
+
+        print(llstr)
+
+lists=LinkedList()
+for x in range(len(given_list)):
+    listfromend=given_list[len(given_list)-1-x]
+    lists.insert_at_beginning(listfromend)
+    print(listfromend)
+
+print(lists.print)
