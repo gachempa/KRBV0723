879bd659a6c90ce5ebcd0a9223b87f5e0d77fa44
gachempa <jpsg514@gmail.com>
2024-03-10 13:41:57 -0700

    set intersection - works

--- LeetPracticeEtc/MatrixIntersection.py
+++ LeetPracticeEtc/MatrixIntersection.py
@@ -1,7 +1,24 @@
 nums1 = [4,9,5]
 nums2 = [9,4,9,8,4]
 
-result=set(nums1) and set(nums2)
+# solution 1, easy and simple using intersection of sets
+s_nums1=set(nums1)
+s_nums2=set(nums2)
+result=s_nums1.intersection(s_nums2)
 
 print(result)
 
+
+# # solution 2
+# mp={}
+# for num in nums1:
+#     mp[num]=mp.get(num,0)+1
+
+# result=[]
+
+# for num in nums2:
+#     if num in mp:
+#         result.append(num)
+#         del mp[num]
+
+# print(result)
