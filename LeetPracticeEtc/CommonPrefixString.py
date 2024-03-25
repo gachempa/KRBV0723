db4982a8f48a15497d14f1796be41537f13943ae
gachempa <jpsg514@gmail.com>
2024-03-25 08:36:02 -0700

    got logic

--- LeetPracticeEtc/CommonPrefixString.py
+++ LeetPracticeEtc/CommonPrefixString.py
@@ -3,13 +3,14 @@ strs=["flower","flow","flight"]
 
 len_strs=len(strs)
 print(len_strs)
+print(strs[0][0])
 
 if len(strs)==0:
     print("")
 else:
     commonindex=0
-    for y in range(len(strs[1]))
-        for x in range(len_strs-1):
-            if strs[y][x]==strs[y][x+1]:
-                print(x)
-            # else: print(common_index) 
+
+    for y in range(len(strs[0])):
+        print(len(strs[0]))
+        to_match=strs[0][y]
+        print([idx for idx in strs if idx[y]==to_match])
