68cec4a4adfba5995ebdf61add469692cb61c968
gachempa <jpsg514@gmail.com>
2024-03-24 22:26:05 -0700

    to be  done

--- LeetPracticeEtc/CommonPrefixString.py
+++ LeetPracticeEtc/CommonPrefixString.py
@@ -1,10 +1,15 @@
 strs=["flower","flow","flight"]
+# strs=[]
 
 len_strs=len(strs)
 print(len_strs)
-common_index=0
 
-for x in range(len_strs):
-    if len(strs)>0:
-       print(x)
-    else: print(common_index) 
+if len(strs)==0:
+    print("")
+else:
+    commonindex=0
+    for y in range(len(strs[1]))
+        for x in range(len_strs-1):
+            if strs[y][x]==strs[y][x+1]:
+                print(x)
+            # else: print(common_index) 
