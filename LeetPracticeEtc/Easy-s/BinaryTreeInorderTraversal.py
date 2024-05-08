b01d2fb3ac4851edf09317774afe2ec1fcd45fd3
gachempa <jpsg514@gmail.com>
2024-05-07 21:58:25 -0700

    playing around

--- LeetPracticeEtc/Easy-s/BinaryTreeInorderTraversal.py
+++ LeetPracticeEtc/Easy-s/BinaryTreeInorderTraversal.py
@@ -6,7 +6,30 @@ class TreeNode:
         self.right = right
 
     def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
         print(self.val)
+        if self.right:
+            self.right.PrintTree()
 
-root=TreeNode(10)
+    def insert(self,val):
+        if self.val:
+            if val<self.val:
+                if self.left is None:
+                    self.left=TreeNode(val)
+                else:
+                    self.left.insert(val)
+            elif val>self.val:
+                if self.right is None:
+                    self.right=TreeNode(val)
+                else:
+                    self.right.insert(val)
+        else:
+            self.val=val
+
+root=TreeNode(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+root.insert(15)
 root.PrintTree()
