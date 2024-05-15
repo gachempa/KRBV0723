afc0b225b3ee0765fe62eb521ca92b8832f97aa7
gachempa <jpsg514@gmail.com>
2024-05-14 18:24:55 -0700

    binary tree work

--- LeetPracticeEtc/Easy-s/BinaryTreeBasics.py
+++ LeetPracticeEtc/Easy-s/BinaryTreeBasics.py
@@ -1,19 +1,18 @@
 
 
-# Creating binary tree  
-# from given list 
+# Creating binary tree from given list 
 from binarytree import build 
-  
-  
+
 # List of nodes 
-nodes =[3, 6, 8, 2, 11, None, 13] 
+nodes =[3, 6, 8, 2, 11, None, 13,4,3,None,2,None,None,3,4,None,None,5] 
   
 # Building the binary tree 
 binary_tree = build(nodes) 
-print('Binary tree from list :\n', 
-      binary_tree) 
+print('Binary tree from list :\n', binary_tree) 
   
-# Getting list of nodes from 
-# binarytree 
-print('\nList from binary tree :',  
-      binary_tree.values) 
+# Getting list of nodes from binarytree 
+print('\nList from binary tree :', binary_tree.values) 
+
+print(binary_tree.max_node_value)
+print(binary_tree.height)
+print(binary_tree.max_leaf_depth)
