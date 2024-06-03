86f8c4ab1e9e2260e0719e685555b7ffac59be11
gachempa <jpsg514@gmail.com>
2024-06-03 11:37:01 -0700

    pascal 1 done

--- LeetPracticeEtc/Set_2/Pascals_triangle.py
+++ LeetPracticeEtc/Set_2/Pascals_triangle.py
@@ -1,13 +1,17 @@
-import copy
+
 numRows=5
 
 pList=[[1]]
+# t1=list(pList[len(pList)-1])
+# t1.append(0)
+
+# print(t1)
 
 if numRows>1:
     for i in range(1,numRows):
-        p1=pList[len(pList)-1]
+        p1=list(pList[len(pList)-1])
         p2=list(p1)
         p2.append(0)
         p1.insert(0,0)
-
-        print(p1,p2)
+        pList.append([sum(x) for x in zip(p1, p2)])
+print(pList)
