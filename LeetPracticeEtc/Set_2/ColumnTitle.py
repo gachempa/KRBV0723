7a4ac38c25b655d8892ceaa08f18e54c22db6161
gachempa <jpsg514@gmail.com>
2024-06-07 14:49:47 -0700

    column number

--- LeetPracticeEtc/Set_2/ColumnTitle.py
+++ LeetPracticeEtc/Set_2/ColumnTitle.py
@@ -1,14 +1,16 @@
 import math
 # columnNumber=2**31 - 1
 columnNumber=10701
+# columnNumber=701
+s=""
 
-quo=int(columnNumber/26)
-rem=columnNumber%26
-
-print(quo)
-print(rem)
+while columnNumber:
+    columnNumber,rem=divmod(columnNumber-1,26)
+    s+=chr(65+rem)
+    print(chr(65+rem))
+print(s[::-1])
 
 # x log 26 = log (2**31 - 1)
 
-x= math.log10(2**31 - 1) / math.log10(26)
-print(x)
+# x= math.log10(2**31 - 1) / math.log10(26)
+# print(x)
